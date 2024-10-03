5c9396bf29d89139a4e105531dcd41245ccfed2e
Yaroslav Grachev <tuulwq@gmail.com>
2024-10-03 13:58:29 +0300

    feat: fee buffer

--- scripts/build-chains.js
+++ scripts/build-chains.js
@@ -63,6 +63,7 @@ function fillAssetData(chain) {
       precision: asset.precision,
       type: asset.type || 'native',
       priceId: asset.priceId,
+      feeBuffer: allowedAsset.feeBuffer || 1,
       icon: replaceUrl(asset.icon, 'asset', asset.symbol),
       typeExtras: replaceTypeExtras(asset.typeExtras, chain.chainId),
     });
